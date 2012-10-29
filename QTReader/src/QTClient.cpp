6f576aebc335ffbc57e6af4b0f8a81997a37f521
Peaches491 <Peaches491@gmail.com>
2012-10-29 19:50:38 -0400

    Commit for Kevin.

--- QTReader/src/QTClient.cpp
+++ QTReader/src/QTClient.cpp
@@ -17,5 +17,26 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
+	struct addrinfo hints;
+	struct addrinfo *res;
+	int sockfd;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo("127.0.0.1", "2012", &hints, &res);
+
+	return 0;
+
+	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+	// connect!
+
+	int result = connect(sockfd, res->ai_addr, res->ai_addrlen);
+
+	printf("Connection Result: %i\n", result);
+
 	return 0;
 }
