28e439be6aa647820723173edf0356c0836a1244
Peaches491 <Peaches491@gmail.com>
2012-11-17 01:42:30 -0500

    I THINK WE'RE DONE.

--- WireView/wireview.cpp
+++ WireView/wireview.cpp
@@ -4,16 +4,19 @@
 #include <iostream>
 #include <pcap/pcap.h>
 #include <map>
+#include <sstream>
+#include <cmath>
 #include "wireview.h"
 
 #include <net/ethernet.h>
 #include <net/if_arp.h>
 
-//#include <netinet/if_ether.h>	//ethernet ARP
+#include <netinet/if_ether.h>	//ethernet ARP
 #include <netinet/ether.h>
 #include <netinet/in.h>		//port definitions, may well not need these
 #include <netinet/ip.h>		//IP
 #include <netinet/tcp.h>	//
+#include <netinet/udp.h>
 /*
  useful stuff:
  net/ethernet.h:
@@ -58,27 +61,6 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
-
-void printStats() {
-	string prefix("    ");
-
-	cout << endl;
-	printf("Total number of Packets: %i\n", packetCount);
-	printf("Start Time: %li\n", (long int) startTime->tv_sec);
-
-	cout << "\nEthernet Source Statistics" << endl;
-	printMap(ethSrcMap, "    %-17s \t%i", 50);
-
-	cout << "\nEthernet Destination Statistics" << endl;
-	printMap(ethDstMap, "    %-17s \t%i", 50);
-
-	cout << "\nIP Source Statistics" << endl;
-	printMap(ipSrcMap, "    %-15s \t%i", 50);
-
-	cout << "\nIP Destination Statistics" << endl;
-	printMap(ipDstMap, "    %-15s \t%i", 50);
-}
-
 int processPCAP(pcap_t* cap) {
 	cout << "pcap_datalink: ";
 	if (pcap_datalink(cap) != DLT_EN10MB) {
@@ -95,17 +77,80 @@ int processPCAP(pcap_t* cap) {
 	return result;
 }
 
+void printStats() {
+	string prefix("    ");
+
+	cout << endl;
+	printf("Total number of Packets: %i\n", packetCount);
+	printf("Maximum Packet Size: %i bytes\n", maxPacketSize);
+	printf("Minimum Packet Size: %i bytes\n", minPacketSize);
+	printf("Average Packet Size: %i\n\n", (int)round(((float)(packetSizeSum)) / ((float)(packetCount))));
+
+	printf("Start Time: ");
+	printTime(startTime);
+	printf("End Time: ");
+	printTime(endTime);
+
+	if ((endTime->tv_usec - startTime->tv_usec) > 0) {
+		durationTime->tv_sec = endTime->tv_sec - startTime->tv_sec;
+		durationTime->tv_usec = endTime->tv_usec - startTime->tv_usec;
+	} else {
+		durationTime->tv_sec = (endTime->tv_sec - startTime->tv_sec) - 1;
+		durationTime->tv_usec = (endTime->tv_usec + 1000000)
+				- startTime->tv_usec;
+	}
+
+	printf("Capture Duration: ");
+	printTimeSeconds(durationTime);
+	printf("\n");
+
+	cout << "\nEthernet Source Statistics" << endl;
+	printMap(ethSrcMap, "    %-17s \t\t%i", 50);
+
+	cout << "\nEthernet Destination Statistics" << endl;
+	printMap(ethDstMap, "    %-17s \t\t%i", 50);
+
+	cout << "\nIP Source Statistics" << endl;
+	printMap(ipSrcMap, "    %-15s \t\t%i", 50);
+
+	cout << "\nIP Destination Statistics" << endl;
+	printMap(ipDstMap, "    %-15s \t\t%i", 50);
+
+	cout << "\nUDP Source Ports" << endl;
+	printMapInt(udpSrcPortsMap, "    %-5s \t\t\t%i", 50);
+
+	cout << "\nUDP Destination Statistics" << endl;
+	printMapInt(udpDstPortsMap, "    %-5s \t\t\t%i", 50);
+
+	cout << "\nARP Request Statistics" << endl;
+	printMapMap(arpReqMap, "    %-17s \t%i", 50);
+
+	cout << "\nARP Receive Statistics" << endl;
+	printMapMap(arpRcvMap, "    %-17s \t%i", 50);
+}
+
 void handler(u_char* user, struct pcap_pkthdr* pkthdr, u_char* pdata) {
 	packetCount++;
+	int packetSize = pkthdr->len;
 
-	//timeval* res = new timeval;
-	// If this packet capture time is older than the start time, update the start time
+	if(packetSize > maxPacketSize){
+		maxPacketSize = packetSize;
+	}
+	if(packetSize < minPacketSize){
+		minPacketSize = packetSize;
+	}
+	packetSizeSum += packetSize;
 
 	if (startTime->tv_sec == 0) {
-		startTime = &(pkthdr->ts);
+		memcpy(startTime, &(pkthdr->ts), sizeof(timeval));
 	} else if (timeCompare(&(pkthdr->ts), (timeval*) startTime) == 1) {
-		printf("UPDATING TIME");
-		startTime = &(pkthdr->ts);
+		memcpy(startTime, &(pkthdr->ts), sizeof(timeval));
+	}
+
+	if ((endTime->tv_sec == 0) && (endTime->tv_usec == 0)) {
+		memcpy(endTime, &(pkthdr->ts), sizeof(timeval));
+	} else if (timeCompare((timeval*) endTime, &(pkthdr->ts))) {
+		memcpy(endTime, &(pkthdr->ts), sizeof(timeval));
 	}
 
 	const ether_header* ethernet = (ether_header*) (pdata);
@@ -114,28 +159,35 @@ void handler(u_char* user, struct pcap_pkthdr* pkthdr, u_char* pdata) {
 
 	int type = ntohs(ethernet->ether_type);
 
-//	printf("Typecode: %x\n", type);
-//	printf("IP      : %x\n", ETHERTYPE_IP);
-
 	const ip* ip_header = (ip*) (pdata + sizeof(ether_header));
+	const ether_arp* arp_header = (ether_arp*) (pdata + sizeof(ether_header));
+	const udphdr* udp_header = (udphdr*) (pdata + sizeof(ether_header) + sizeof(ip));
+
 	char* addr = niceIPaddr((in_addr*) &(ip_header->ip_dst), false);
 
 	switch (type) {
 	case ETHERTYPE_ARP:
 		//printf("ARP\n");
+
+		arpReqMap[niceMACaddr((u_int8_t*) arp_header->arp_sha)][niceIPaddrA(
+				(u_int8_t*) (arp_header->arp_spa))]++;
+		arpRcvMap[niceMACaddr((u_int8_t*) arp_header->arp_tha)][niceIPaddrA(
+				(u_int8_t*) (arp_header->arp_tpa))]++;
+
 		break;
 	case ETHERTYPE_IP:
+		addr = niceIPaddr((in_addr*) &(ip_header->ip_dst), false);
 		ipDstMap[addr]++;
 		addr = niceIPaddr((in_addr*) &(ip_header->ip_src), false);
 		ipSrcMap[addr]++;
 
 		switch (ip_header->ip_p) {
 		case IPPROTO_UDP:
-
+			udpSrcPortsMap[ntohs(udp_header->source)]++;
+			udpDstPortsMap[ntohs(udp_header->dest)]++;
 			break;
 		case IPPROTO_TCP:
-			//printf("\t* Found TCP packet\n");
-			//cout << TCPpayload(pdata, size_ip) << endl;
+		default:
 			break;
 		}
 		break;
@@ -144,7 +196,6 @@ void handler(u_char* user, struct pcap_pkthdr* pkthdr, u_char* pdata) {
 		break;
 	}
 
-	//if(ip->protocol == IPPro)
 }
 
 char* TCPpayload(const u_char* pdata, u_int size_ip) {
@@ -164,46 +215,26 @@ char* TCPpayload(const u_char* pdata, u_int size_ip) {
 }
 
 int timeCompare(timeval* x, timeval* y) {
-	if (x->tv_usec < y->tv_usec) {
-		int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-		y->tv_usec -= 1000000 * nsec;
-		y->tv_sec += nsec;
-	}
-	if (x->tv_usec - y->tv_usec > 1000000) {
-		int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-		y->tv_usec += 1000000 * nsec;
-		y->tv_sec -= nsec;
-	}
-
-	/* Return 1 if result is negative. */
-	return x->tv_sec < y->tv_sec;
-}
-int timeCompare(timeval* x, timeval* y, timeval* result) {
-	if (x->tv_usec < y->tv_usec) {
-		int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-		y->tv_usec -= 1000000 * nsec;
-		y->tv_sec += nsec;
-	}
-	if (x->tv_usec - y->tv_usec > 1000000) {
-		int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-		y->tv_usec += 1000000 * nsec;
-		y->tv_sec -= nsec;
+	if ((x->tv_sec == y->tv_sec) && (x->tv_usec == y->tv_usec)) {
+		return 0;
+	} else if ((x->tv_sec == y->tv_sec)) {
+		if ((x->tv_usec < y->tv_usec)) {
+			return 1;
+		} else {
+			return -1;
+		}
+	} else if (x->tv_sec < y->tv_sec) {
+		return 1;
+	} else {
+		return -1;
 	}
 
-	/* Compute the time remaining to wait.
-	 tv_usec is certainly positive. */
-	result->tv_sec = x->tv_sec - y->tv_sec;
-	result->tv_usec = x->tv_usec - y->tv_usec;
-
-	/* Return 1 if result is negative. */
-	return x->tv_sec < y->tv_sec;
 }
 
 //create a traditional human-readable IPv4 address
 char* niceIPaddr(in_addr* addr) {
 	return niceIPaddr(addr, false);
 }
-//create a traditional human-readable IPv4 address
 char* niceIPaddr(in_addr* addr, bool print) {
 	char* ip = new char[16];
 	memset(ip, 0, sizeof(ip));
@@ -216,12 +247,18 @@ char* niceIPaddr(in_addr* addr, bool print) {
 
 	return ip;
 }
+char* niceIPaddrA(u_int8_t* nums) {
+	char* ip = new char[16];
+	memset(ip, 0, sizeof(ip));
+	sprintf(ip, "%i.%i.%i.%i", nums[0], nums[1], nums[2], nums[3]);
+	//printf("%s", ip);
+	return ip;
+}
 
 //create a traditional human-readable MAC address
 char* niceMACaddr(u_int8_t addr[6]) {
 	return niceMACaddr(addr, false);
 }
-//create a traditional human-readable MAC address
 char* niceMACaddr(u_int8_t addr[6], bool print) {
 	char* mac = new char[17];
 	memset(mac, 0, sizeof(char) * 17);
@@ -240,8 +277,7 @@ int printMap(map<string, int> map, string format, int bufSize) {
 	int i = 0;
 	char* buffer = new char[bufSize];
 
-	typedef std::map<std::string, int> map_type;
-	map_type::const_iterator iter;
+	string_int_map::const_iterator iter;
 	for (iter = map.begin(); iter != map.end(); ++iter) {
 		snprintf(buffer, bufSize, format.c_str(), iter->first.c_str(),
 				iter->second);
@@ -251,4 +287,67 @@ int printMap(map<string, int> map, string format, int bufSize) {
 	snprintf(buffer, bufSize, format.c_str(), "  Total", i);
 	cout << buffer << endl;
 	return i;
+}
+
+int printMapInt(map<int, int> map, string format, int bufSize) {
+	int i = 0;
+	char* buffer = new char[bufSize];
+
+	int_int_map::const_iterator iter;
+	for (iter = map.begin(); iter != map.end(); ++iter) {
+		stringstream ss;
+		ss << iter->first;
+		snprintf(buffer, bufSize, format.c_str(), (char*)ss.str().c_str(),
+				iter->second);
+		cout << buffer << endl;
+		i += iter->second;
+	}
+	snprintf(buffer, bufSize, format.c_str(), "  Total", i);
+	cout << buffer << endl;
+	return i;
+}
+
+
+int printMapMap(std::map<std::string, std::map<std::string, int> > map,
+		std::string format, int bufSize) {
+	int macs = 0;
+	int addresses = 0;
+	char* buffer = new char[bufSize];
+
+	string_string_int_map::const_iterator iter_out;
+	for (iter_out = map.begin(); iter_out != map.end(); ++iter_out) {
+
+		addresses = 0;
+
+		snprintf(buffer, bufSize, "    %s", iter_out->first.c_str());
+		cout << buffer << endl;
+
+		string_int_map::const_iterator iter_in;
+		string_int_map inside_map = iter_out->second;
+		for (iter_in = inside_map.begin(); iter_in != inside_map.end();
+				++iter_in) {
+			snprintf(buffer, bufSize, ("  " + format).c_str(),
+					iter_in->first.c_str(), iter_in->second);
+			cout << buffer << endl;
+			addresses += iter_in->second;
+		}
+		snprintf(buffer, bufSize, ("  " + format).c_str(), "  IPs ", addresses);
+		cout << buffer << endl << endl;
+		macs++;
+	}
+	snprintf(buffer, bufSize, ("  " + format).c_str(), "  MACs ", macs);
+	cout << buffer << endl;
+	return macs;
+}
+
+void printTime(timeval* time) {
+	char timeBuf[56];
+	memset(timeBuf, 0, sizeof(timeBuf));
+	strftime(timeBuf, sizeof(timeBuf), "%a %B %d, %Y %X",
+			localtime((time_t*) &(time->tv_sec)));
+	printf("%s.%li\n", timeBuf, time->tv_usec);
+}
+
+void printTimeSeconds(timeval* time) {
+	printf("%li.%li sec", time->tv_sec, time->tv_usec);
 }
--- WireView/wireview.h
+++ WireView/wireview.h
@@ -16,17 +16,27 @@
 
 #define ETHERNET_LENGTH 14
 
-int packetCount = 0; 					// Total number of packets.
-timeval* startTime = new timeval;						// Start date and time of the packet capture.
-timeval* durationTime = new timeval;					// Duration of the packet capture in seconds with microsecond resolution.
-std::map<std::string, int> ipSrcMap;	// Create two lists, one for unique senders and one for unique recipients, along with the total number of packets associated with each.
-std::map<std::string, int> ipDstMap;	//    This should be done at two layers: Ethernet and IP. For Ethernet, represent the addresses in hex-colon notation. For IP addresses, use the standard dotted decimal notation.
-std::map<std::string, int> ethSrcMap;
-std::map<std::string, int> ethDstMap;
+typedef std::map<int, int> int_int_map;
+typedef std::map<std::string, int> string_int_map;
+typedef std::map<std::string, std::map<std::string, int> > string_string_int_map;
 
-std::map<std::string, int> UDPsrcPorts;
-std::map<std::string, int> UDPdstPorts;
-std::map<std::string, int> ARPlist;
+int packetCount = 0; 					// Total number of packets.
+int minPacketSize = 0x7fffffff;
+int maxPacketSize = 0;
+long int packetSizeSum = 0;
+timeval* startTime = new timeval;		// Start date and time of the packet capture.
+timeval* endTime = new timeval;
+timeval* durationTime = new timeval;	// Duration of the packet capture in seconds with microsecond resolution.
+string_int_map ipSrcMap;	// Create two lists, one for unique senders and one for unique recipients, along with the total number of packets associated with each.
+string_int_map ipDstMap;	// This should be done at two layers: Ethernet and IP. For Ethernet, represent the addresses in hex-colon notation. For IP addresses, use the standard dotted decimal notation.
+string_int_map ethSrcMap;
+string_int_map ethDstMap;
+
+
+int_int_map udpSrcPortsMap;
+int_int_map udpDstPortsMap;
+string_string_int_map arpReqMap;
+string_string_int_map arpRcvMap;
 
 // list of machines participating in ARP,
 // 		including their associated MAC addresses and,
@@ -34,11 +44,16 @@ std::map<std::string, int> ARPlist;
 // For UDP, create two lists for the unique ports seen: one for the source ports and one for the destination ports.
 // Report the average, minimum, and maximum packet sizes. The packet size refers to everything beyond the tcpdump header.
 
+void printTime(timeval* time);
+void printTimeSeconds(timeval* time);
 void printStats();
 int printMap(std::map<std::string, int> map, std::string format, int bufSize);
+int printMapInt(std::map<int, int> map, std::string format, int bufSize);
+int printMapMap(std::map<std::string, std::map<std::string, int> > map, std::string format, int bufSize);
 int processPCAP(pcap_t* cap);
 char* niceIPaddr(in_addr* addr);
 char* niceIPaddr(in_addr* addr, bool print);
+char* niceIPaddrA(u_int8_t* nums);
 char* niceMACaddr(u_int8_t addr[6]);
 char* niceMACaddr(u_int8_t addr[6], bool print);
 void handler(u_char *user, struct pcap_pkthdr *phrd, u_char *pdata);
