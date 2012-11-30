11c02556df068b5a666eff2e61c1a7db2e11cf15
Peaches491 <Peaches491@gmail.com>
2012-11-30 00:08:15 -0500

    Added packet printing

--- OLNet/OLRouter.cpp
+++ OLNet/OLRouter.cpp
@@ -5,8 +5,12 @@
 //============================================================================
 
 #include <iostream>
+#include <arpa/inet.h>
 #include "OLPacket.h"
 using namespace std;
+
+void dropPacket(packet* p);
+void printPacket(packet* p);
 
 int main(int argc, char* argv[]) {
 
@@ -17,17 +21,16 @@ int main(int argc, char* argv[]) {
 
 	bool router = false;
 
-	for (int i = 1; i < argc; i++) {
-
-		if ((i) != argc) {
-			// Host Tag
-			if (strcmp(argv[i], "-h") == 0) {
-				router = false;
-				// Router Tag
-			} else if (strcmp(argv[i], "-r") == 0) {
-				router = true;
-			}
-		}
+	// Host Tag
+	if (strcmp(argv[1], "-h") == 0) {
+		router = false;
+		// Router Tag
+	} else if (strcmp(argv[1], "-r") == 0) {
+		router = true;
+	} else {
+		cout << "ERROR: Argument 1 must specify either -r (for router mode) -h (for host mode)" << endl;
+		cout << endl;
+		return -1;
 	}
 
 	if (router == true) {
@@ -38,14 +41,23 @@ int main(int argc, char* argv[]) {
 		char* testWord = (char*) "0123456789";
 
 		packet* p = new packet;
+		p->header.ip_header.ip_ttl = 12;
+		if (p->header.ip_header.ip_ttl != 0) {
+			p->header.ip_header.ip_ttl -= 1;
+		} else {
+			dropPacket(p);
+		}
 		p->data = testWord;
 		p->header.dataSize = strlen(p->data) + 1;
-
-
-
 		cout << endl;
+
 		int sock = create_cs3516_socket();
-		int sent = sendPacket(sock, p, 0);
+
+		unsigned int addr = 0;
+		inet_pton(AF_INET, "192.168.132.164", &addr);
+
+		int sent = sendPacket(sock, p, addr);
+
 		cout << "Sent: " << sent << " bytes" << endl;
 
 //		char* buf = new char[1000];
@@ -68,6 +80,8 @@ int main(int argc, char* argv[]) {
 		char* buf = new char[1000];
 		int recv = cs3516_recv(sock, buf, 1000);
 
+		packet* p = ((packet*) buf);
+
 		cout << endl;
 
 		cout << "Recv: " << recv << " bytes" << endl;
@@ -76,9 +90,23 @@ int main(int argc, char* argv[]) {
 		cout << "Data Size: " << ((packet*) buf)->header.dataSize << " bytes"
 				<< endl;
 
+		printPacket(p);
+
 		char* data = buf + sizeof(packethdr);
 		cout << data << endl;
 
 		return 0;
 	}
+}
+
+void dropPacket(packet* p){
+	cout << "DROPPING PACKET" << endl;
+	printPacket(p);
+}
+
+void printPacket(packet* p){
+	cout << "PACKET INFO: " << endl;
+	cout << "\tOL Source: " << inet_ntoa(p->header.ip_header.ip_src) << endl;
+	cout << "\tOL Dest:   " << inet_ntoa(p->header.ip_header.ip_dst) << endl;
+	cout << "\tOL TTL:    " << inet_ntoa(p->header.ip_header.ip_dst) << endl;
 }
